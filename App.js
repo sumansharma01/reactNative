95169697193872ab5512f439233706f5ea9a17a9
sumansharma01 <suman.sharma0331@gmail.com>
2021-04-24 13:23:18 +0530

    added lists and scrollview

--- App.js
+++ App.js
@@ -1,38 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, ScrollView} from 'react-native';
 
 const App=()=> {
-  const [name,setName]=useState('');
-  const [age,setAge]=useState()
+  const [person,setPerson]=useState([
+    {name:'ab',key:1},
+    {name:'cd',key:'2'},
+    {name:'ef',key:'3'},
+    {name:'gh',key:'4'},
+    {name:'ij',key:'5'},
+    {name:'kl',key:'6'},
+    {name:'mn',key:'7'},
+    {name:'op',key:'8'},
+    {name:'qr',key:'9'},
+    {name:'st',key:'10'},
+    {name:'uv',key:'11'},
+    {name:'wx',key:'12'},
+    {name:'yz',key:13},
+  ])
   
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text>React-native app</Text>
-      </View>
-      <View style={styles.body}>
-        
-        <Text>Enter name:</Text>
-        <TextInput 
-          style={styles.input}
-          onChangeText={(value)=>{setName(value)}}
-        />
-
-        <Text>Enter age:</Text>
-        <TextInput 
-          keyboardType='number-pad'
-          style={styles.input}
-          onChangeText={(value)=>{setAge(value)}}
-        />
-
-        <Text>name:{name} age:{age}</Text>
-        <View >
-          <Button title="Click me"/>
-        </View>
-
-      </View>
       
+      <ScrollView>
+
+        {person.map(p=>{
+          return(
+            
+            <View key={p.key}>
+            <Text style={[styles.mapP,styles.boldText]}>{p.name}</Text>
+            </View>
+          )
+          
+        })}
+
+      </ScrollView>
       
       <StatusBar style="auto" />
     </View>
@@ -43,29 +45,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop:50,
+    marginHorizontal:20,
+    
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  mapP:{
+    marginBottom:20,
+    backgroundColor:'pink',
+    padding:30,
+    fontSize:25
   },
   boldText:{
     fontWeight:'bold'
-  },
-  header:{
-    backgroundColor:'red',
-    padding:20,
-  },
-  body:{
-    backgroundColor:'white',
-    padding:20,
-  },
-  bodyText:{
-    color:'red',
-    
-  },
-  input:{
-    borderColor:'gray',
-    borderWidth:1,
-    padding:2,
-    width:300
   }
   
 });
