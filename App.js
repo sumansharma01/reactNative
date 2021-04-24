29cb6fa83c553d38ef9601051afcde9b879dcd81
sumansharma01 <suman.sharma0331@gmail.com>
2021-04-24 12:29:33 +0530

    added a few view and text nodes

--- App.js
+++ App.js
@@ -2,10 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App=()=> {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text>React-native app</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={[styles.boldText, styles.bodyText]}>{Math.ceil(Math.random()*10)}</Text>
+      </View>
+      
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +25,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  boldText:{
+    fontWeight:'bold'
+  },
+  header:{
+    backgroundColor:'red',
+    padding:20,
+  },
+  body:{
+    backgroundColor:'black',
+    padding:20,
+  },
+  bodyText:{
+    color:'red',
+    
+  }
+  
 });
+
+export default App;
