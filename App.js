a59f655d012b54611b8e762ad3caead74889db09
sumansharma01 <suman.sharma0331@gmail.com>
2021-04-24 13:43:04 +0530

    added flatlist

--- App.js
+++ App.js
@@ -1,28 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, ScrollView} from 'react-native';
+import { StyleSheet, Text, View, FlatList} from 'react-native';
 
 const App=()=> {
   const [person,setPerson]=useState([
-    {name:'ab',key:1},
-    {name:'cd',key:'2'},
-    {name:'ef',key:'3'},
-    {name:'gh',key:'4'},
-    {name:'ij',key:'5'},
-    {name:'kl',key:'6'},
-    {name:'mn',key:'7'},
-    {name:'op',key:'8'},
-    {name:'qr',key:'9'},
-    {name:'st',key:'10'},
-    {name:'uv',key:'11'},
-    {name:'wx',key:'12'},
-    {name:'yz',key:13},
+    {name:'ab',id:1},
+    {name:'cd',id:'2'},
+    {name:'ef',id:'3'},
+    {name:'gh',id:'4'},
+    {name:'ij',id:'5'},
+    {name:'kl',id:'6'},
+    {name:'mn',id:'7'},
+    {name:'op',id:'8'},
+    {name:'qr',id:'9'},
+    {name:'st',id:'10'},
+    {name:'uv',id:'11'},
+    {name:'wx',id:'12'},
+    {name:'yz',id:'13'},
   ])
   
   return (
     <View style={styles.container}>
       
-      <ScrollView>
+      {/* <ScrollView>
 
         {person.map(p=>{
           return(
@@ -34,7 +34,21 @@ const App=()=> {
           
         })}
 
-      </ScrollView>
+      </ScrollView> */}
+
+
+      {/* Flatlist is better to use considering the performance, also can use numColumns */}
+      {/* Flatlist just loads data upto which the content could be scrolled while map loads all
+      hence the better performance  */}
+
+      <FlatList 
+        numColumns={2}
+        keyExtractor={(item)=>{return(item.id)}}
+        data={person}
+        renderItem={({item})=>{return(
+          <Text style={[styles.mapP,styles.boldText]}>{item.name}</Text>)
+        }}
+      />
       
       <StatusBar style="auto" />
     </View>
@@ -55,7 +69,9 @@ const styles = StyleSheet.create({
     marginBottom:20,
     backgroundColor:'pink',
     padding:30,
-    fontSize:25
+    fontSize:25,
+    marginHorizontal:10,
+    width:165
   },
   boldText:{
     fontWeight:'bold'
